5cbf9224462230b4cc32743e7bf895e3d80737df
jayb <jayabaaskarb@gmail.com>
2026-06-09 00:27:05 +0530

    Remote backend

--- providers.tf
+++ providers.tf
@@ -6,6 +6,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "fanvault-v2-tfstate-899071933396"
+    key            = "fanvault-v2/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "fanvault-v2-tfstate-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
