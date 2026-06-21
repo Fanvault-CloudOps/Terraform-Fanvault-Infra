cd57444baa915227f7a3200f1d91918a9cfd3363
JayabaaskarB <137640547+Savitxr@users.noreply.github.com>
2026-06-21 13:47:01 +0530

    use lockfile

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "fanvault-tfstate-dev-899071933396"
-    key            = "environments/dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "fanvault-tf-locks-dev"
-    encrypt        = true
+    bucket       = "fanvault-tfstate-dev-899071933396"
+    key          = "environments/dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
