3460ddf2766dd3d98a2a4925b95e93cc8cc4c88c
jayabaaskarb <jayabaaskarb@gmail.com>
2026-06-22 05:23:51 +0000

    tf apply

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "fanvault-tfstate-prod-773384830607"
-    key            = "environments/prod/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "fanvault-tf-locks-prod"
-    encrypt        = true
+    bucket       = "fanvault-tfstate-prod-773384830607"
+    key          = "environments/prod/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
